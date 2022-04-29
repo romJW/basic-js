bc586399e9b9f29668a44442a3bf0fbd121b60b3
Roman Bukharin <buxarin1999@gmail.com>
2022-04-29 16:19:52 +0600

    add a task

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date="dateAbsence") {
+  if(date === "dateAbsence" ) return 'Unable to determine the time of year!';
+  try {
+
+    date.getTime()
+  
+  } catch (e) {
+    throw new Error('Invalid date!');
+  }
+
+  
+  
+  if (date instanceof Date  ){ 
+if (Object.prototype.toString.call(date) !== '[object Date]' ) throw new Error('Invalid date!');
+else if (date.getMonth()<2 || date.getMonth() == 11 || date =="winter" ) return "winter";
+else if (date.getMonth()>=2 & date.getMonth()<5 || date =="spring") return "spring";
+else if (date.getMonth()>=5 & date.getMonth()<8  || date =="spring") return "summer";
+else if (date.getMonth()>=8 & date.getMonth()<11 || date =="autumn") return "autumn";
+  }
+   throw new Error('Invalid date!');
 }
 
 module.exports = {
